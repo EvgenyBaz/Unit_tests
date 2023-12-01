ae460d70f9bcf2a88cc2dfd1d5964ef716fa3dd9
EvgenyBaz <evgeny.bazarov@gmail.com>
2023-12-01 19:59:32 +0300

    HomeWork04 done

--- HomeWork04/test_book_service.py
+++ HomeWork04/test_book_service.py
@@ -1,33 +1,12 @@
-import unittest
-from unittest.mock import Mock
 from book_service import BookService, Book, BookRepository
-
-#
-# class TestBookService(unittest.TestCase):
-#     def setUp(self) -> None:
-#         self.service = BookService(Mock())
-#
-#     def test_find_by_id(self):
-#         book_id = 3
-#         self.service.find_by_id(book_id)
-#         self.service.book_repository.find_by_id.assert_called_with(book_id)
-#
-#     def test_find_all(self):
-#         self.service.find_all()
-#         self.service.book_repository.find_all.assert_called_once()
-
-# import unittest
-# from unittest.mock import Mock
-# from book import BookService, Book, BookRepository
 import unittest
 from unittest.mock import Mock
-from book_service import BookService
 
 
 class BookServiceTest(unittest.TestCase):
 
     def setUp(self):
-        self.bookRepository = Mock()
+        self.bookRepository = Mock(BookRepository)
         self.bookService = BookService(self.bookRepository)
 
     def test_find_book_by_id(self):
